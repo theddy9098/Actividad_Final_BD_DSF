724bf68296284fdb7343c1794ea747fcfb1835ac
teddy <teddyfso15@gmail.com>
2025-06-30 22:46:43 -0500

    Creacion de la conexion con la base de datos en este caso se uso posgretSQL

--- src/main/java/org/CRUD_BD/Infraestructura/Confi_BD/Conexion_BD.java
+++ src/main/java/org/CRUD_BD/Infraestructura/Confi_BD/Conexion_BD.java
@@ -1,4 +1,22 @@
 package org.CRUD_BD.Infraestructura.Confi_BD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion_BD {
+    private static final String URL = "jdbc:postgresql://localhost:5432/ruta_escolar";
+    private static final String USUARIO = "Teddy_XYZ";
+    private static final String CONTRASENA = "1111";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        return connection;
+    }
 }
--- src/main/java/org/CRUD_BD/Main.java
+++ src/main/java/org/CRUD_BD/Main.java
@@ -1,17 +1,28 @@
 package org.CRUD_BD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Parámetros de conexión
+            String url = "jdbc:postgresql://localhost:5432/crud_bd";
+            String usuario = "Teddy_xyz";
+            String password = "1111";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            try {
+                // Intentar establecer la conexión
+                Connection conexion = DriverManager.getConnection(url, usuario, password);
+                System.out.println("¡Conexión exitosa a la base de datos!");
+
+
+
+            } catch (SQLException e) {
+                System.out.println("Error al conectar a la base de datos");
+                e.printStackTrace();
+            }
         }
-    }
 }
